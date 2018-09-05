1e651e7b12056de69ba008c8918d7f99889fc2ab
reznikovkg <reznikov.kosta@yandex.ru>
2018-09-05 14:55:36 +0300

    maps

--- frontend/src/components/Page/Maps/Map.vue
+++ frontend/src/components/Page/Maps/Map.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="content">
         <div id="map"></div>
-
-        <button type="submit" @click="getMarkers">go</button>
-        <!-- Replace the value of the key parameter with your own API key. -->
     </div>
 </template>
 
@@ -15,22 +12,52 @@
         data () {
             return {
                 map: null,
-                markers5: null
+                markers5: null,
+
+                lat: null,
+                lng: null
+
             }
         },
         mounted: function () {
-            this.map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: -34.397, lng: 150.644},
-                zoom: 8
-            });
+            this.initMap();
+            this.getMarkers();
         },
         methods: {
+            initMap: function () {
+                this.map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: -34.397, lng: 150.644},
+                    zoom: 8
+                });
+
+                google.maps.event.addListener(this.map, 'click', (event) => {
+                    console.log(event.latLng.lat());
+                    this.lat = event.latLng.lat();
+                    this.lng = event.latLng.lng();
+                    var marker = new google.maps.Marker({
+                        position: event.latLng,
+                        map: this.map
+                    });
+                });
+            },
             getMarkers: function () {
                 axios.get(`${this.$root.domain}/api/maps/allMarkers`)
                     .then((response)=>{
+
+
                         this.markers5 = response.data.markers;
+
+                        var markers = this.markers5;
+
+                        markers.forEach((item, i, markers) => {
+                            var uluru = {lat: item.lat, lng: item.lng};
+                            var marker = new google.maps.Marker({position: uluru, map: this.map});
+                        });
+
+
+
                     });
-            }
+            },
         }
 
     }
